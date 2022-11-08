14433a7253af90e93a9423f47bbc1ecbdc2b134e
Gabriel Nascimento <gabrielnascimento_17@hotmail.com>
2022-11-08 02:25:07 -0300

    fix process

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
 
-app.listen(proccess.env.PORT || 8080, () => console.log(`App listen in port: http://localhost:8080`))
+app.listen(process.env.PORT || 8080, () => console.log(`App listen in port: http://localhost:8080`))
 
 const executeRequest = (req, res) => {
 
