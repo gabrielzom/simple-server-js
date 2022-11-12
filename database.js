7542b73478e1eebb479ea01745fdc030803d1310
Gabriel Nascimento <gabrielnascimento_17@hotmail.com>
2022-11-12 13:28:04 -0300

    fix: pool of connections config

--- database.js
+++ database.js
@@ -3,12 +3,17 @@ import { config } from 'dotenv'
 config()
 
 const connectToDatabase = async () => {
+  if(global.connection) {
+    console.log('Stop here')
+    return global.connection
+  }
   const connection = await mysql.createConnection({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PWD
   })
+  global.connection = connection
   console.log('MySQL connected')
   return connection
 }
