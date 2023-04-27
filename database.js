ce860421e7a9cd343c18dbd0e291b3d0b7fb80c8
Gabriel Nascimento <gabrielnascimento_17@hotmail.com>
2023-04-26 23:28:44 -0300

    changed

--- database.js
+++ database.js
@@ -2,23 +2,29 @@ import mysql from 'mysql2/promise'
 import { config } from 'dotenv'
 config()
 
-const connectToDatabase = async () => {
-  if(global.connection) {
-    console.log('MySQL already connected')
+export const connectToDatabase = async () => {
+  if (global.connection) {
+    console.log('MYSQL ALREADY CONNECTED')
     return global.connection
   }
-  const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD
-  })
-  global.connection = connection
-  console.log('MySQL connected')
-  return connection
+  try {
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PWD
+    })
+    global.connection = connection
+    console.log('MYSQL CONNECTED')
+    return connection
+  } catch (e) {
+    console.log('MYSQL CONNECTION HAS OCCURRED AN ERROR: ' + e.message)
+    throw new SQLException()
+  }
 }
 
 export const executeScript = async script => {
   const connection = await connectToDatabase()
-  return await connection.query(script)
+  return connection.query(script);
 }
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import cors from 'cors'
-import { executeScript } from './database.js'
+import { executeScript, connectToDatabase } from './database.js'
 import { config } from 'dotenv'
 config()
 
-const app = express();
+const app = express()
 
 app.use(cors())
 app.use(express.json())
@@ -13,27 +13,26 @@ app.use(express.urlencoded({extended : false}))
 app.listen(process.env.PORT || 8080, () => console.log(`App listen in port: http://localhost:8080`))
 
 const executeRequest = (req, res) => {
-
   const scriptsObjectPayload = req.body
   const scriptsList = Object
     .keys(scriptsObjectPayload)
     .map(script => scriptsObjectPayload[script])
-
-    
   if (['/start-trip', '/end-trip', '/make-payment']
       .includes(req.path)
     ) {
-    Promise.all(scriptsList.map(script => executeScript(script)))
+    return Promise.all(scriptsList.map(script => executeScript(script)))
       .then(result => res.status(200).json({
         rows: result[result.length-1][0],
         columns: result[result.length-1][1],
       }))
-  } else {
-    Promise.all(scriptsList.map(script => executeScript(script)))
-      .then(result => res.status(200).json({rows: result[0][0], columns: result[0][1]}))
-      .catch(error => res.status(500).json(error))
   }
-
+  Promise.all(scriptsList.map(script => executeScript(script)))
+    .then(result =>
+      res.status(200).json({rows: result[0][0], columns: result[0][1]})
+    )
+    .catch(error =>
+      res.status(500).json(error)
+    )
 }
 
 app.route('/client').post((req, res) => executeRequest(req, res))
